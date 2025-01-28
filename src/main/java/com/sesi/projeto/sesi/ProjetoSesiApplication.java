1b7ee348bd329a8d49dea8c2132b208668fba746
Daniel <dogualtieroni@gmail.com>
2025-01-28 10:12:54 -0300

    :wrench: chore: Estrutura inicial do Projeto

---
+++ src/main/java/com/sesi/projeto/sesi/ProjetoSesiApplication.java
@@ -0,0 +1,13 @@
+package com.sesi.projeto.sesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sesi/projeto/sesi/ProjetoSesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sesi.projeto.sesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
